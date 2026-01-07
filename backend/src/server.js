ebefc6f0adfc3770f01a9048952aa50903b5def5
Sakshi <sakshibattala6@gmail.com>
2026-01-07 18:13:13 +0530

    deployement changes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
 import { ENV } from "./libs/env.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-app.listen(ENV.port, () => console.log("server is listening on port " + ENV.port));
+if (ENV.node_env === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.port, () =>
+  console.log("server is listening on port " + ENV.port)
+);
